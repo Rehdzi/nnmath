c535f32027e86aa9a06b87dacc5187fc751e52d3
Rehdzi <5crkurwa@gmail.com>
2022-03-02 19:48:37 +0300

    Simple layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,46 +7,17 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <header>
+    <h1>NNMath</h1>
+  </header>
+  <main>
+    <div class="bg-gray-600 flex">
+      <div class="w-1/5 rounded-xl bg-purple-400 p-14 m-4">
+        <p>nav</p>
+      </div>
+      <div class="w-4/5 rounded-xl bg-green-400 p-14 m-4">
+        <p>content</p>
+      </div>
+    </div>
+  </main>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
